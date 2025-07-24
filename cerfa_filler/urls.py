e63858369f189c07d17c39c86c2d5f9945453a64
lpofredc <frederic.cloitre@lpo.fr>
2025-07-24 17:48:13 +0200

    generate pdf

--- cerfa_filler/urls.py
+++ cerfa_filler/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CompaniesCerfa
+from .views import CompaniesCerfa, CompaniesCerfaToPdf
 
 urlpatterns = [
     path("cerfa/<int:pk>", CompaniesCerfa.as_view(), name="companies-cerfa"),
+    path("cerfa/pdf/<int:pk>", CompaniesCerfaToPdf.as_view(), name="companies-cerfa"),
 ]
--- cerfa_filler/views.py
+++ cerfa_filler/views.py
@@ -1,10 +1,49 @@
-from django.shortcuts import render
 from django.views.generic import DetailView
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from weasyprint import HTML
+from pypdf import PdfReader, PdfWriter
+import io
 
 from .models import Companies
 
+
 class CompaniesCerfa(DetailView):
-    template_name = 'companies_cerfa.svg'
+    template_name = "companies_cerfa.svg"
     model = Companies
 
 
+class CompaniesCerfaToPdf(DetailView):
+    template_name = "companies_cerfa.svg"
+    model = Companies
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+
+        # Generate PDF files from SVG templates
+        pdfs = []
+        for template in ("companies_1.svg", "companies_2.svg"):
+            svg_content = render_to_string(template, context)
+            pdf_bytes = HTML(string=svg_content).write_pdf()
+            pdfs.append(io.BytesIO(pdf_bytes))  # Use BytesIO to create a file-like object
+
+        # Merge the generated PDF files
+        writer = PdfWriter()
+        for pdf in pdfs:
+            reader = PdfReader(pdf)
+            for page in reader.pages:
+                writer.add_page(page)
+
+        # Create a response with the merged PDF
+        response = HttpResponse(content_type="application/pdf")
+        response["Content-Disposition"] = 'attachment; filename="output.pdf"'
+        
+        # Write the merged PDF to the response
+        output_pdf = io.BytesIO()  # Create a BytesIO object for the output
+        writer.write(output_pdf)
+        output_pdf.seek(0)  # Move to the beginning of the BytesIO stream
+
+        response.write(output_pdf.read())  # Write the PDF content to the response
+
+        return response
