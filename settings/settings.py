ccebcc395ed6267cfcc401a837c43863f6b8f2ef
lpofredc <frederic.cloitre@lpo.fr>
2026-02-11 11:52:06 +0100

    icon

--- settings/settings.py
+++ settings/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS += [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -119,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = config("LANGUAGE_CODE", default="fr-FR")
+LANGUAGE_CODE = config("LANGUAGE_CODE", default="fr")
 
 TIME_ZONE = config("TIME_ZONE", default="Europe/Paris")
 
